a82340bc28251f52dee53f108a562f27a938662d
modeverv <modeverv@gmail.com>
2019-12-15 03:28:07 +0900

    a

---
+++ Debug/src/alds1_3_b_queue.d
@@ -0,0 +1,1 @@
+src/alds1_3_b_queue.o: ../src/alds1_3_b_queue.cpp
---
+++ src/alds1_3_b_queue.cpp
@@ -0,0 +1,60 @@
+//============================================================================
+// Name        : alds1_3_b_queue.cpp
+// Author      : lovesaemi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#define LEN 100005
+using namespace std;
+
+typedef struct pp {
+	char name[100];
+	int t;
+} P;
+
+P Q[LEN];
+int head, tail, n;
+void enqueue(P x) {
+	Q[tail] = x;
+	tail = (tail + 1) % LEN;
+}
+P dequeue() {
+	P x = Q[head];
+	head = (head + 1) % LEN;
+	return x;
+}
+
+int min(int a, int b) {
+	return a < b ? a : b;
+}
+
+int main() {
+	int elaps = 0, c;
+	int i, q;
+	P u;
+	scanf("%d %d", &n, &q);
+	for (i = 1; i <= n; i++) {
+		scanf("%s", Q[i].name);
+		scanf("%d", &Q[i].t);
+	}
+	head = 1;
+	tail = n + 1;
+	while (head != tail) {
+		u = dequeue();
+		c = min(q, u.t);
+		u.t -= c;
+		elaps += c;
+		if (u.t > 0) {
+			enqueue(u);
+		} else {
+			printf("%s %d\n", u.name, elaps);
+		}
+	}
+
+	return 0;
+}
